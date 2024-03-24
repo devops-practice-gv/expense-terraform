31a4fe47e2b0d0e37897e6de063bb76a080408d7
Gaurav <gauravenigma@gmail.com>
2024-03-24 00:55:32 +0000

    Expense app Terraform

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -21,7 +21,7 @@ resource "null_resource" "ansible" {
 
     inline = [
       "sudo pip3.11 install ansible",
-      "ansible-pull -i localhost, -U https://github.com/devops-practice-gv/expense-ansible expense.yml -e env=£(var.env) -e role_name=$component expense.yml",
+      "ansible-pull -i localhost, -U https://github.com/devops-practice-gv/expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component} ",
 
 
     ]
@@ -30,7 +30,7 @@ resource "null_resource" "ansible" {
 }
 
 resource "aws_route53_record" "record" {
-  name    = "$(var.component)-$(var.env)"
+  name    = "${var.component}-${var.env}"
   type    = "A"
   zone_id = var.zone_id
   records = [aws_instance.instance.private_ip]
