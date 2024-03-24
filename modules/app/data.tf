b4d7d13a10ab38fa0a0a1fd7c06386b3150dee76
Gaurav <gauravenigma@gmail.com>
2024-03-24 01:08:51 +0000

    Expense app Terraform

--- modules/app/data.tf
+++ modules/app/data.tf
@@ -1,7 +1,8 @@
 data "aws_ami" "ami" {
   most_recent      = true
   name_regex       = "RHEL-9-DevOps-Practice"
-  owners           = ["972946952941"]
+#  owners           = ["972946952941"]   this is not owner ID, it the ID of the AMI name maybe
+  owners           = ["973714476881"]
 }
 
 data "aws_security_group" "security_group" {
