c8b981956d1fa916de00543486732db2fabff71d
Gaurav <gauravenigma@gmail.com>
2024-03-25 11:28:31 +0000

    1. Framework for tools infra 2. Created added prometheus to create infra 3. created a pipeline for go cd

--- main.tf
+++ main.tf
@@ -1,14 +1,6 @@
 #Calling  ./modules/app module for frontend ******************
 
-module "frontend" {
-  source = "./modules/app"
-  instance_type = var.instance_type
-  component = "frontend"
-  ssh_user = var.ssh_user
-  ssh_pass = var.ssh_pass
-  env = var.env
-  zone_id = var.zone_id
-}
+
 
 #Practice/trial code **************************
 #module "app2" {
@@ -39,4 +31,14 @@ module "backend" {
   ssh_pass      = var.ssh_pass
   env           = var.env
   zone_id       = var.zone_id
+}
+
+module "frontend" {
+  source = "./modules/app"
+  instance_type = var.instance_type
+  component = "frontend"
+  ssh_user = var.ssh_user
+  ssh_pass = var.ssh_pass
+  env = var.env
+  zone_id = var.zone_id
 }
